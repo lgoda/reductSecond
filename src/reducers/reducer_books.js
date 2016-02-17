4ace754f66e11eb5c2de45fd2ec096faaf5168db
Luca Godano <luca.godano@gmail.com>
2016-02-17 23:09:10 +0100

    first commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    {title: 'Javascript: The Good Parts', pages:101},
+    {title: 'Harry Potter', pages: 29},
+    {title: 'The dark Tower', pages: 56},
+    {title: 'Eloquent Ruby', pages:24}
+  ]
+}
